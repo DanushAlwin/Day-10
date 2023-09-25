88b6669a484e4c964af61c071ec782758e3bd9c6
DanushAlwin <120357066+DanushAlwin@users.noreply.github.com>
2023-09-25 15:12:52 +0530

    Add files via upload

---
+++ Fi nal/src/App.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import Dashboard from './pages/Dashboard'
+import Users from './pages/Users'
+import Settings from './pages/Settings'
+import Home from './pages/Home'
+import WateringSchedule from './pages/WateringSchedule'
+import Intro from './pages/Intro'
+import WeatherApp from './pages/weather'
+import PlantIdentification from './pages/PlantIdentification'
+import Results from './pages/results'
+import Pp from './pages/pp'
+import Faq from './pages/Faq'
+
+function App() {
+  return (
+   <>
+   <Routes>
+    <Route exact path='/home' element={<Home/>}/>
+    <Route exact path='/' element={<Login/>}/>
+    <Route exact path='/Register' element={<Register/>}/>
+    <Route exact path='/Dashboard'element={<Dashboard/>} />
+    <Route exact path='/Users' element={<PlantIdentification />}/>
+    <Route exact path='/Settings' element={<Settings/>}/>
+    <Route exact path='/Watering' element={<WateringSchedule/>}/>
+    <Route exact path='/Intro' element={<Intro/>}/>
+    <Route exact path='/weather' element = {<WeatherApp />}></Route>
+    {/* <Route exact path='/search' element = {}></Route> */}
+    <Route exact path='/res' element = {<Results />}></Route>
+    <Route exact path='/privacy-policy' element = {<Pp />}></Route>
+    <Route exact path='/about-us' element = {<Faq />}></Route>
+   </Routes>
+   
+   </>
+  )
+}
+
+export default App
